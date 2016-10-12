1d55cd32c4861b469f6967652605bd539cb5430c
cibernox <miguel.camba@gmail.com>
2016-10-12 20:42:01 +0100

    Render month with surrounding days

--- addon/components/power-calendar.js
+++ addon/components/power-calendar.js
@@ -1,23 +1,54 @@
-import Ember from 'ember';
 import layout from '../templates/components/power-calendar';
+import Component from 'ember-component';
 import computed from 'ember-computed';
+import moment from 'moment';
 
-export default Ember.Component.extend({
+export default Component.extend({
   layout,
-  month: 1,
+  onlyCurrent: false,
+  monthNumber: 0,
   // CPs
-  days: computed(function() {
-    return Array(...Array(30)).map((_, i) => ({ number: ++i }));
+  monthName: computed('monthNumber', function() {
+    return moment().month(this.get('monthNumber')).format('MMMM');
   }),
 
-  weeks: computed('days', function() {
-    let days = this.get('days');
+  weeks: computed('monthNumber', function() {
+    let monthNumber = this.get('monthNumber');
+    let month = moment().month(monthNumber);
+    let beginOfMonth = month.clone().startOf('month');
+    let startOfFirstWeek = beginOfMonth.clone().startOf('isoWeek');
+    let endOfMonth = month.clone().endOf('month');
+    let endOfLastWeek = endOfMonth.clone().endOf('isoWeek');
+    let currentMoment = startOfFirstWeek.clone();
+    let days = [];
+    while (currentMoment.isBefore(endOfLastWeek)) {
+      days.push({
+        number: currentMoment.date(),
+        currentMonth: currentMoment.month() === monthNumber
+      });
+      currentMoment.add(1, 'day');
+    }
+    let weeks = new Array(days.length / 7);
     let i = 0;
-    let weeks = [];
     while (days[i]) {
-      weeks.push(days.slice(i, i + 7));
+      weeks[i] = days.slice(i, i + 7);
       i += 7;
     }
     return weeks;
-  })
+  }),
+
+  // Actions
+  actions: {
+    prev() {
+      let newNumber = this.get('monthNumber') - 1;
+      if (newNumber < 0) {
+        newNumber += 11;
+      }
+      this.set('monthNumber', newNumber);
+    },
+    next() {
+      let newNumber = this.get('monthNumber') + 1 % 11;
+      this.set('monthNumber', newNumber);
+    }
+  }
 });
