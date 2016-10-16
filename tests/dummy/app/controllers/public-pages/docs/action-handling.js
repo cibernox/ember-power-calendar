43c39775b1ec2aac83a7a0e7f4a7bac8720e9c99
cibernox <miguel.camba@gmail.com>
2016-10-16 23:54:40 +0100

    Explain usage of async month change

--- tests/dummy/app/controllers/public-pages/docs/action-handling.js
+++ tests/dummy/app/controllers/public-pages/docs/action-handling.js
@@ -1,9 +1,10 @@
 import Controller from 'ember-controller';
+import { task, timeout } from 'ember-concurrency';
 
 export default Controller.extend({
-  actions: {
-    updateMonth(newMonth) {
-      // debugger;
-    }
-  }
+  updateMonth: task(function* (newMonth) {
+    yield timeout(600); // Pretend this is an ajax call to the server
+    // here we update the events
+    this.set('displayedMonth', newMonth);
+  }).drop()
 });
