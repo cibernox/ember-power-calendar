69fdd043bfdd2d7c65bff9c60612ee238b28afb4
cibernox <miguel.camba@gmail.com>
2016-10-20 16:15:50 +0100

    Add example of crazy things you can do with onchange

---
+++ tests/dummy/app/templates/snippets/multiple-selection-1-js.js
@@ -0,0 +1,7 @@
+import Controller from 'ember-controller';
+import moment from 'moment';
+
+export default Controller.extend({
+  displayedMonth: moment('2016-05-17'),
+  collection: []
+});
---
+++ tests/dummy/app/templates/snippets/multiple-selection-2-js.js
@@ -0,0 +1,27 @@
+import Controller from 'ember-controller';
+import moment from 'moment';
+
+const primes = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
+export default Controller.extend({
+  displayedMonth: moment('2016-05-17'),
+  selectedPrimes: [],
+
+  actions: {
+    selectClosestPrime(moments) {
+      let lastOne = moments[moments.length - 1];
+      let number = lastOne.date();
+      if (primes.indexOf(number) === -1) {
+        let nextPrimeIndex = primes.findIndex((p) => p > number);
+        let nextPrime = primes[nextPrimeIndex];
+        let prevPrime = primes[nextPrimeIndex - 1];
+        let closestPrime = nextPrime - number < number - prevPrime ? nextPrime : prevPrime;
+        if (moments.some((m) => m.date() === closestPrime)) {
+          moments.pop()
+        } else {
+          lastOne.date(closestPrime);
+        }
+      }
+      this.set('selectedPrimes', moments);
+    }
+  }
+});;
