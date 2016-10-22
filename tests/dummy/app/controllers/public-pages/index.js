f67293834ab9955feb9f7c08b4f157b7e1e4c762
cibernox <miguel.camba@gmail.com>
2016-10-23 00:25:42 +0100

    Finished icons

---
+++ tests/dummy/app/controllers/public-pages/index.js
@@ -0,0 +1,10 @@
+import Controller from 'ember-controller';
+
+export default Controller.extend({
+  today: null,
+
+  init() {
+    this._super(...arguments);
+    this.set('today', moment());
+  },
+})
