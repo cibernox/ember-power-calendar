c4a422e25a81d09bddd65ce3570ebdec4e1aa65f
cibernox <miguel.camba@gmail.com>
2016-10-24 13:49:25 +0100

    Documented better how to customize the days component

---
+++ tests/dummy/app/templates/snippets/the-days-2-js.js
@@ -0,0 +1,32 @@
+import Controller from 'ember-controller';
+import computed from 'ember-computed';
+import moment from 'moment';
+
+export default Controller.extend({
+  days: computed(function() {
+    let now = moment();
+    let day = now.clone().startOf('month').startOf('isoWeek');
+    let lastDay = now.clone().endOf('month').endOf('isoWeek');
+    let days = [];
+    while (day.isBefore(lastDay)) {
+      if (day.weekday() !== 1 && day.weekday() !== 3) { // Skip Mon/Wed
+        let copy = day.clone();
+        let isCurrentMonth = copy.month() === now.month();
+        days.push({ date: copy._d, moment: copy, isCurrentMonth });
+      }
+      day.add(1, 'day');
+    }
+    return days;
+  }),
+
+  weeksWithoutMondaysOrWednesday: computed('noMondays', function() {
+    let days = this.get('days');
+    let weeks = [];
+    let i = 0;
+    while (days[i]) {
+      weeks.push({ days: days.slice(i, i + 5) });
+      i += 5;
+    }
+    return weeks;
+  })
+});
