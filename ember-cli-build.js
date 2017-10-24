70f083090e741984e958f7759908849a2d356e98
cibernox <miguel.camba@gmail.com>
2017-10-24 22:01:45 +0200

    Update all dependencies preparing for test helpers refactor

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,8 +1,9 @@
 /* eslint-env node */
+'use strict';
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 module.exports = function(defaults) {
-  let project = defaults.project;
+  let { project } = defaults;
   let options = {
     snippetPaths: ['tests/dummy/app/templates/snippets']
   };
--- tests/helpers/module-for-acceptance.js
+++ tests/helpers/module-for-acceptance.js
@@ -1,9 +1,7 @@
 import { module } from 'qunit';
-import RSVP from 'rsvp';
+import { resolve } from 'rsvp';
 import startApp from '../helpers/start-app';
 import destroyApp from '../helpers/destroy-app';
-
-const { Promise } = RSVP;
 
 export default function(name, options = {}) {
   module(name, {
@@ -17,7 +15,7 @@ export default function(name, options = {}) {
 
     afterEach() {
       let afterEach = options.afterEach && options.afterEach.apply(this, arguments);
-      return Promise.resolve(afterEach).then(() => destroyApp(this.application));
+      return resolve(afterEach).then(() => destroyApp(this.application));
     }
   });
 }
