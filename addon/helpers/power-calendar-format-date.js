4b48a5e11f32bf8a5f622ce20e200a485db9e774
Miguel Camba <miguel.camba@gmail.com>
2018-08-12 18:02:16 +0200

    Add {{power-calendar-format-date}} helper for internal use only

---
+++ addon/helpers/power-calendar-format-date.js
@@ -0,0 +1,8 @@
+import { helper } from '@ember/component/helper';
+import { formatDate } from "ember-power-calendar/utils/date-utils";
+
+export function powerCalendarFormatDate([date, format], { locale }) {
+  return formatDate(date, format, locale);
+}
+
+export default helper(powerCalendarFormatDate);
