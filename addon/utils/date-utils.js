9dca5390f18ae8b1bed0e27328f011a043d6ee1f
Miguel Camba <miguel.camba@gmail.com>
2018-08-19 15:34:32 +0200

    Save progress

--- addon/utils/date-utils.js
+++ addon/utils/date-utils.js
@@ -1,87 +1,87 @@
-export function add(date, quantity, unit) {
+export function add(/*date, quantity, unit*/) {
   throw new Error('Not implemented');
 }
 
-export function formatDate(date, format, locale = null) {
+export function formatDate(/*date, format, locale = null*/) {
   throw new Error("Not implemented");
 }
 
-export function startOf(date, unit) {
+export function startOf(/*date, unit*/) {
   throw new Error('Not implemented');
 }
 
-export function endOf(date, unit) {
+export function endOf(/*date, unit*/) {
   throw new Error('Not implemented');
 }
 
-export function weekday(date) {
+export function weekday(/*date*/) {
   throw new Error('Not implemented');
 }
 
-export function isoWeekday(date) {
+export function isoWeekday(/*date*/) {
   throw new Error('Not implemented');
 }
 
-export function getWeekdaysShort() {
+export function getWeekdaysShort(/**/) {
   throw new Error('Not implemented');
 }
 
-export function getWeekdaysMin() {
+export function getWeekdaysMin(/**/) {
   throw new Error('Not implemented');
 }
 
-export function getWeekdays() {
+export function getWeekdays(/**/) {
   throw new Error('Not implemented');
 }
 
-export function isAfter(date1, date2) {
+export function isAfter(/*date1, date2*/) {
   throw new Error('Not implemented');
 }
 
-export function isBefore(date1, date2) {
+export function isBefore(/*date1, date2*/) {
   throw new Error('Not implemented');
 }
 
-export function isSame(date1, date2, unit) {
+export function isSame(/*date1, date2, unit*/) {
   throw new Error('Not implemented');
 }
 
-export function isBetween(date, start, end, unit, inclusivity) {
+export function isBetween(/*date, start, end, unit, inclusivity*/) {
   throw new Error('Not implemented');
 }
 
-export function diff(date1, date2) {
+export function diff(/*date1, date2*/) {
   throw new Error('Not implemented');
 }
 
-export function normalizeDate(dateOrMoment) {
+export function normalizeDate(/*dateOrMoment*/) {
   throw new Error("Not implemented");
 }
 
-export function normalizeRangeActionValue(val) {
+export function normalizeRangeActionValue(/*val*/) {
   throw new Error("Not implemented");
 }
 
-export function normalizeMultipleActionValue(val) {
+export function normalizeMultipleActionValue(/*val*/) {
   throw new Error("Not implemented");
 }
 
-export function normalizeCalendarDay(day) {
+export function normalizeCalendarDay(/*day*/) {
   throw new Error("Not implemented");
 }
 
-export function withLocale(locale, fn) {
-  throw new Error("Not implemented");;
-}
-
-export function normalizeCalendarValue(value) {
+export function withLocale(/*locale, fn*/) {
   throw new Error("Not implemented");
 }
 
-export function normalizeDuration(value) {
+export function normalizeCalendarValue(/*value*/) {
   throw new Error("Not implemented");
 }
 
-export function getDefaultLocale() {
+export function normalizeDuration(/*value*/) {
+  throw new Error("Not implemented");
+}
+
+export function getDefaultLocale(/**/) {
   throw new Error("Not implemented");
 }
