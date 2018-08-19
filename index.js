a4ca73208de2de73edb08f3f36eb63b483c53621
Miguel Camba <miguel.camba@gmail.com>
2018-08-19 23:55:33 +0200

    Throw error if EPC is installed but epc-moment and epc-luxon are not installed

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-power-calendar'
+  name: 'ember-power-calendar',
+
+  included() {
+    let { registry: { availablePlugins } } = this._findHost()
+    this._super.included.apply(this, arguments);
+    if (!availablePlugins.hasOwnProperty('ember-power-calendar-moment') && !availablePlugins.hasOwnProperty('ember-power-calendar-luxon')) {
+      throw new Error(`You have installed "ember-power-calendar" but you don't have any of the required meta-addons to make it work. Please, explicitly install 'ember-power-calendar-moment' or 'ember-power-calendar-luxon' in your app`);
+    }
+  }
 };
