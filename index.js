c3ac42c55062db55e4cb1cf6b61bdfa5d8fb1070
Miguel Camba <miguel.camba@gmail.com>
2018-08-20 11:39:33 +0200

    Consider ember-power-calendar-moment and ember-power-calendar-luxon exceptions

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ module.exports = {
   included() {
     let { registry: { availablePlugins } } = this._findHost()
     this._super.included.apply(this, arguments);
+    if (['ember-power-calendar-moment', 'ember-power-calendar-moment'].includes(this.project.name())) {
+      return;
+    }
     if (!availablePlugins.hasOwnProperty('ember-power-calendar-moment') && !availablePlugins.hasOwnProperty('ember-power-calendar-luxon')) {
       throw new Error(`You have installed "ember-power-calendar" but you don't have any of the required meta-addons to make it work. Please, explicitly install 'ember-power-calendar-moment' or 'ember-power-calendar-luxon' in your app`);
     }
