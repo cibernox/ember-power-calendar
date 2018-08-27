9c78248dd75415e1a01bc130e59dc9a88e300491
Alex Aralis <alex.aralis@oath.com>
2018-08-27 13:47:24 -0500

    fix some moment deps

--- tests/integration/components/power-calendar-test.js
+++ tests/integration/components/power-calendar-test.js
@@ -504,7 +504,7 @@ module('Integration | Component | Power Calendar', function(hooks) {
 
   test('If the user passes `maxDate=someDate` to single calendars, days after that one cannot be selected, but that day and those days before can', async function(assert) {
     assert.expect(6);
-    this.maxDate = moment('2013-10-15');
+    this.maxDate = new Date(2013, 9, 15);
     await render(hbs`
       {{#power-calendar selected=selected onSelect=(action (mut selected) value="date") as |calendar|}}
         {{calendar.nav}}
@@ -516,7 +516,7 @@ module('Integration | Component | Power Calendar', function(hooks) {
     assert.dom('.ember-power-calendar-day[data-date="2013-10-15"]').isNotDisabled('The maxDate is selectable');
     assert.dom('.ember-power-calendar-day[data-date="2013-10-16"]').isDisabled('Days after the maxDate are disabled');
 
-    run(() => this.set('maxDate', moment('2013-10-18')));
+    run(() => this.set('maxDate', new Date(2013, 9, 18)));
     assert.dom('.ember-power-calendar-day[data-date="2013-10-14"]').isNotDisabled('Days before the minDate are selectable');
     assert.dom('.ember-power-calendar-day[data-date="2013-10-18"]').isNotDisabled('The maxDate is selectable');
     assert.dom('.ember-power-calendar-day[data-date="2013-10-19"]').isDisabled('Days after the maxDate are disabled');
