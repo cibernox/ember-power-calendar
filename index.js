54f4401ad81af13fe3122041277532a1f02e82ad
Jiří Prokop <jiri-prokop@synaptiko.cz>
2018-12-12 18:54:51 +0100

    Fix dependency check which is broken under yarn workspaces (#181)

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ module.exports = {
   name: 'ember-power-calendar',
 
   included() {
-    let { registry: { availablePlugins } } = this._findHost()
     this._super.included.apply(this, arguments);
     if (['ember-power-calendar-moment', 'ember-power-calendar-luxon'].includes(this.project.name())) {
       return;
     }
-    if (!availablePlugins.hasOwnProperty('ember-power-calendar-moment') && !availablePlugins.hasOwnProperty('ember-power-calendar-luxon')) {
+
+    const hostAppAddons = this.project.addonPackages;
+    const parentAddons = this.parent.addonPackages;
+
+    const hasMetaAddon = addons => addons.hasOwnProperty('ember-power-calendar-moment') || addons.hasOwnProperty('ember-power-calendar-luxon');
+
+    if (!hasMetaAddon(hostAppAddons) && !hasMetaAddon(parentAddons)) {
       throw new Error(`You have installed "ember-power-calendar" but you don't have any of the required meta-addons to make it work. Please, explicitly install 'ember-power-calendar-moment' or 'ember-power-calendar-luxon' in your app`);
     }
   }
