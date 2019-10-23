0ab135e8c3e73664d7a551ea47933c2e4d4fe6c7
Jen Weber <j@jenweber.me>
2019-10-23 14:20:18 -0400

    Add generation of CSS-only styles

---
+++ compile-css.js
@@ -0,0 +1,19 @@
+/*
+This turns SCSS into CSS that can be used in projects that don't use a
+precompiler.
+*/
+
+var sass = require('sass'); // eslint-disable-line
+var fs = require('fs');
+var path = require('path');
+
+var inputFile = path.join(__dirname, 'app', 'styles', 'ember-power-calendar.scss');
+var outputFile = path.join(__dirname, 'vendor', 'ember-power-calendar.css');
+var buf = fs.readFileSync(inputFile, "utf8");
+
+var result = sass.renderSync({
+  data: buf,
+  includePaths: ['app/styles/ember-power-calendar-defaults.scss']
+});
+
+fs.writeFileSync(outputFile, result.css);
