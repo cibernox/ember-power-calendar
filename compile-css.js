a2fd58f01e4e8cc89c85dfce32f46f5e34f572fd
Jen Weber <j@jenweber.me>
2019-10-23 17:30:55 -0400

    update compile-css filepaths

--- compile-css.js
+++ compile-css.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 /*
 This turns SCSS into CSS that can be used in projects that don't use a
 precompiler.
@@ -13,7 +15,7 @@ var buf = fs.readFileSync(inputFile, "utf8");
 
 var result = sass.renderSync({
   data: buf,
-  includePaths: ['app/styles/ember-power-calendar-defaults.scss']
+  includePaths: ['app/styles/ember-power-calendar.scss']
 });
 
 fs.writeFileSync(outputFile, result.css);
