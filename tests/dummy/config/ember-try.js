ed17c97044dea74efc56a7d1bd33992ed3821c78
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-08-09 16:43:45 +0200

    Remove travis.yml & add update ember-try

--- tests/dummy/config/ember-try.js
+++ tests/dummy/config/ember-try.js
@@ -8,9 +8,21 @@ module.exports = async function () {
     useYarn: true,
     scenarios: [
       {
+        name: 'ember-lts-3.28',
+        npm: {
+          devDependencies: {
+            'ember-resolver': '^8.0.0',
+            'ember-source': '~3.28.0',
+            '@ember/test-helpers': '^2.9.3',
+            'ember-qunit': '^6.2.0',
+          },
+        },
+      },
+      {
         name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
+            'ember-resolver': '^8.0.0',
             'ember-source': '~4.4.0',
           },
         },
@@ -20,6 +32,14 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~4.8.0',
+          },
+        },
+      },
+      {
+        name: 'ember-5.0',
+        npm: {
+          devDependencies: {
+            'ember-source': '~5.0.0',
           },
         },
       },
