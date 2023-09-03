960b8a5609e671b54e35acf1a0cb697dc539cfd0
Markus Sanin <markus.sanin@zeppelin-group.com>
2023-09-03 10:26:21 +0200

    Add `registerDateLibrary` to allow register every date library instead of strict packages

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -2,6 +2,10 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'dummy/config/environment';
+import { registerDateLibrary } from 'ember-power-calendar';
+import DateUtils from 'ember-power-calendar-moment';
+
+registerDateLibrary(DateUtils);
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
