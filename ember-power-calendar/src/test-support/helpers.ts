45c804f4a2b6eb5a241b260838e7659a320f617a
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-02-04 11:56:15 +0100

    Convert helpers to ts

--- ember-power-calendar/src/test-support/helpers.ts
+++ ember-power-calendar/src/test-support/helpers.ts
@@ -2,12 +2,15 @@ import { run } from '@ember/runloop';
 import { assert } from '@ember/debug';
 import { click, settled, find } from '@ember/test-helpers';
 import { formatDate } from '../utils.ts';
+import type PowerCalendarMultipleComponent from '../components/power-calendar-multiple.ts';
+import type PowerCalendarComponent from '../components/power-calendar.ts';
+import type PowerCalendarRangeComponent from '../components/power-calendar-range.ts';
 
 export default {};
 
 export * from '../utils.ts';
 
-function findCalendarElement(selector) {
+function findCalendarElement(selector: string): Element | null | undefined {
   let target = find(selector);
 
   if (target) {
@@ -22,7 +25,7 @@ function findCalendarElement(selector) {
   }
 }
 
-function findCalendarGuid(selector) {
+function findCalendarGuid(selector: string): string | undefined {
   let maybeCalendar = findCalendarElement(selector);
   if (!maybeCalendar) {
     return;
@@ -30,20 +33,21 @@ function findCalendarGuid(selector) {
   if (maybeCalendar.classList.contains('ember-power-calendar')) {
     return maybeCalendar.id;
   } else {
-    return maybeCalendar.attributes['data-power-calendar-id'].value;
+    return (maybeCalendar as HTMLElement).dataset['dataPowerCalendarId'];
   }
 }
 
-function findComponentInstance(selector) {
+function findComponentInstance(selector: string): PowerCalendarComponent | PowerCalendarMultipleComponent | PowerCalendarRangeComponent {
   let calendarGuid = findCalendarGuid(selector);
   assert(
     `Could not find a calendar using selector: "${selector}"`,
     calendarGuid,
   );
+  // @ts-expect-error Property '__powerCalendars'
   return window.__powerCalendars[calendarGuid];
 }
 
-export async function calendarCenter(selector, newCenter) {
+export async function calendarCenter(selector: string, newCenter: Date): Promise<void> {
   assert(
     '`calendarCenter` expect a Date object as second argument',
     newCenter instanceof Date,
@@ -55,18 +59,18 @@ export async function calendarCenter(selector, newCenter) {
     !!onCenterChange,
   );
   let publicAPI = calendarComponent.publicAPI;
-  await run(() => publicAPI.actions.changeCenter(newCenter, publicAPI));
+  run(() => publicAPI.actions.changeCenter!(newCenter, publicAPI, {} as MouseEvent));
   return settled();
 }
 
-export async function calendarSelect(selector, selected) {
+export async function calendarSelect(selector: string, selected: Date): Promise<void> {
   assert('`calendarSelect` expect a Date object as second argument', selected);
   let calendarElement = findCalendarElement(selector);
   let daySelector = `${selector} [data-date="${formatDate(
     selected,
     'YYYY-MM-DD',
   )}"]`;
-  let dayElement = calendarElement.querySelector(daySelector);
+  let dayElement = calendarElement?.querySelector(daySelector);
   if (!dayElement) {
     await calendarCenter(selector, selected);
   }
