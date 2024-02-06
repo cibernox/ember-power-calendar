8a917662bf43e5c5626ec3c9f0834115eb522fd5
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-02-06 17:01:03 +0100

    Add glint in docs

--- docs/app/app.ts
+++ docs/app/app.ts
@@ -3,6 +3,7 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'docs/config/environment';
 import { registerDateLibrary } from 'ember-power-calendar';
+// @ts-expect-error Could not find a declaration file for module
 import DateUtils from 'ember-power-calendar-moment';
 
 registerDateLibrary(DateUtils);
---
+++ docs/app/components/calendar-nav-with-year-buttons.ts
@@ -0,0 +1,10 @@
+import templateOnly from '@ember/component/template-only';
+
+export interface CalendarNavWithYearButtonsSignature {
+  Element: Element;
+  Args: {
+    calendar: any;
+  };
+}
+
+export default templateOnly<CalendarNavWithYearButtonsSignature>();
