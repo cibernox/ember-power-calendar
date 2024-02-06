5048da2354cca54f0b8c21b847c3fe89ed0d8af7
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-02-06 17:03:20 +0100

    Add missing package & fix lint

--- docs/types/global.d.ts
+++ docs/types/global.d.ts
@@ -4,11 +4,12 @@ import '@glint/environment-ember-template-imports';
 import type EmberPowerCalendarRegistry from 'ember-power-calendar/template-registry';
 
 export interface CodeSnippetRegistry {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	[key: string]: any;
-  }
-  
-  declare module '@glint/environment-ember-loose/registry' {
-	export default interface Registry
-	  extends EmberPowerCalendarRegistry, CodeSnippetRegistry {}
-  }
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  [key: string]: any;
+}
+
+declare module '@glint/environment-ember-loose/registry' {
+  export default interface Registry
+    extends EmberPowerCalendarRegistry,
+      CodeSnippetRegistry {}
+}
