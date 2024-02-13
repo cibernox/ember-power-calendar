41fde68a1b8109989331a51cea4572224e679b0a
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-02-13 07:43:01 +0100

    Update ember-power-calendar-moment to v0.6

--- docs/app/app.ts
+++ docs/app/app.ts
@@ -3,7 +3,6 @@ import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'docs/config/environment';
 import { registerDateLibrary } from 'ember-power-calendar';
-// @ts-expect-error Could not find a declaration file for module
 import DateUtils from 'ember-power-calendar-moment';
 
 registerDateLibrary(DateUtils);
