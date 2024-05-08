9f29a53cb3717a8769ea3027e2fa0b8b59332003
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-05-08 08:22:16 +0200

    Update addon blueprint

--- ember-power-calendar/src/template-registry.ts
+++ ember-power-calendar/src/template-registry.ts
@@ -1,3 +1,7 @@
+// Easily allow apps, which are not yet using strict mode templates, to consume your Glint types, by importing this file.
+// Add all your components, helpers and modifiers to the template registry here, so apps don't have to do this.
+// See https://typed-ember.gitbook.io/glint/environments/ember/authoring-addons
+
 import type PowerCalendarComponent from './components/power-calendar.ts';
 import type PowerCalendarRangeComponent from './components/power-calendar-range.ts';
 import type PowerCalendarMultipleComponent from './components/power-calendar-multiple.ts';
