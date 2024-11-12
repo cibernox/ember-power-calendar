6f5119e0b2b8faba9c5cf81e810ca1bc4bdcfd55
Markus Sanin <markus.sanin@zeppelin-group.com>
2024-11-12 07:34:11 +0100

    Allow `@glimmer/component` v2 (#530)
    * Allow `@glimmer/component` v2
    
    * Update ember-try
    
    * Try overrides
    
    * Fix ember-try
    
    * Revert change (show be overrides)
    
    * Move @glimmer/component to devDependencies
    
    * Update ci (force re-sync injected deps) & app glimmer-component-v1 test
    
    * Fix ember-try

--- docs/config/ember-try.js
+++ docs/config/ember-try.js
@@ -11,6 +11,7 @@ module.exports = async function () {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
+            '@glimmer/component': '^1.1.2',
             'ember-resolver': '^8.0.0',
             'ember-cli': '~4.12.2',
             'ember-source': '~3.28.0',
@@ -23,6 +24,7 @@ module.exports = async function () {
         name: 'ember-lts-4.4',
         npm: {
           devDependencies: {
+            '@glimmer/component': '^1.1.2',
             'ember-resolver': '^8.0.0',
             'ember-source': '~4.4.0',
           },
@@ -32,6 +34,7 @@ module.exports = async function () {
         name: 'ember-lts-4.8',
         npm: {
           devDependencies: {
+            '@glimmer/component': '^1.1.2',
             'ember-resolver': '^11.0.0',
             'ember-source': '~4.8.0',
           },
@@ -58,6 +61,14 @@ module.exports = async function () {
         npm: {
           devDependencies: {
             'ember-source': '~5.8.0',
+          },
+        },
+      },
+      {
+        name: 'glimmer-component-v1',
+        npm: {
+          devDependencies: {
+            '@glimmer/component': '^1.1.2',
           },
         },
       },
