c26fc3c6ee4585d10d6ecb37cb3e59a44e108008
Markus Sanin <markus.sanin@zeppelin-group.com>
2025-03-31 14:27:26 +0200

    Cleanup @ember/service deprecation (#549)

--- ember-power-calendar/src/components/power-calendar-range.ts
+++ ember-power-calendar/src/components/power-calendar-range.ts
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { inject as service } from '@ember/service';
+import service from '../-private/service.ts';
 import { guidFor } from '@ember/object/internals';
 import { assert } from '@ember/debug';
 import { task } from 'ember-concurrency';
