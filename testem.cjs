07610073730b914c4cacdf3b7e9f8274413f0769
Markus Sanin <markus.sanin@zeppelin-group.com>
2026-04-15 14:50:54 +0200

    Fix test?

--- testem.cjs
+++ testem.cjs
@@ -3,6 +3,7 @@
 if (typeof module !== 'undefined') {
   module.exports = {
     test_page: 'tests/index.html?hidepassed',
+    cwd: 'dist-tests',
     disable_watching: true,
     launch_in_ci: ['Chrome'],
     launch_in_dev: ['Chrome'],
